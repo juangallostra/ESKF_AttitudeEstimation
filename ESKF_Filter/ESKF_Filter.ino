08d59eb7a565957acb1c122adeeeccaf1ab15a88
PepMS <jmarti@bonadrone.com>
2018-12-04 20:30:10 +0100

    Clean up

--- ESKF_Filter/ESKF_Filter.ino
+++ ESKF_Filter/ESKF_Filter.ino
@@ -1,23 +1,3 @@
-/* 
-   Calibrate.ino: LSM6DSM calibrating sketch
-
-   Copyright (C) 2018 Simon D. Levy
-
-   This file is part of LSM6DSM.
-
-   LSM6DSM is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   LSM6DSM is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   You should have received a copy of the GNU General Public License
-   along with LSM6DSM.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "LSM6DSM.h"
 #include <Wire.h>
 #include <math.h>
@@ -104,11 +84,15 @@ void loop()
     
     uint32_t t_delta = t_last - micros();
     t_last = micros();
-    // Update estimation
+    
+    // Update state estimation
     float Fq[16];
     computeFq(&gx, &gy, &gz, t_delta, Fq);
     matrixByVector(4,4,Fq,q,q);
-  
+    normalizeVector(4,q,q);
+
+    // Update error-state estimation
+    float Fdq[3]; 
 
   }
 }
